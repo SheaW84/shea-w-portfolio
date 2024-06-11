bc7924a430d5cdec207643fbcef288a389960448
SheaW84 <sheawilsn@gmail.com>
2024-06-10 20:37:41 -0500

    struggles to get image to load

---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+
+const Main = () => {
+  return (
+    <div id="main">
+      <img className='w-full h-screen object-cover object-left'src="../assets/man_at_the_laptop.jpg" alt="masked man at work on laptop" />
+    </div>
+  )
+}
+
+export default Main
--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import {useState} from 'react'
 import {AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail} from 'react-icons/ai';
 import {BsPerson} from 'react-icons/bs'
 import {GrProjects} from 'react-icons/gr'
